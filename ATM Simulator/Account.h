de869c0d630f7c36eaa5dec369182a965161c6fa
Gintoki734 <khanalaryan2003@gmail.com>
2025-10-29 16:01:57 +0000

    further implementation on account class

--- ATM Simulator/Account.h
+++ ATM Simulator/Account.h
@@ -5,12 +5,19 @@
 //To hold card account information
 struct CardAccount {
 	std::string pin;
-	double balance;
+	double balance; //Use of double allows for a safer and precise value compared to float
 };
 
 class Account
 {
 private:
 	std::unordered_map<std::string, CardAccount> accounts; //A fast way to do lookups
+
+public:
+	bool verifyCard(const std::string& cardNum); // Verify the card, spit out the card if it isnt valid
+	bool verifyPin(const std::string& cardNum, const std::string& pin); // Verify the card, spit out the card if it isnt valid
+	bool withdraw(const std::string& cardNum, double amount); // return false if there isnt sufficient money on the account otherwise true for success
+	double currentbalance(const std::string& cardNum); // Returns the balance of the account
+	void deposit(const std::string& cardNum, double amount); // Deposit money into account
 };
 
